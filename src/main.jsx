15f2e1e9bb1b3957c8c858c24658cb310bfdb93e
danalurie1-cmd <danalurie1@gmail.com>
2025-12-07 16:06:42 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AcoustiSkinCRM from './acoustiskin-crm.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './acoustiskin-crm.jsx'; 
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AcoustiSkinCRM />
+    <App />
   </React.StrictMode>,
-)
+);
